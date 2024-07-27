605c7d83fa5e4ab3911cae83ef7627fc16fe7e30
TinaPera <valentina.pera2007@gmail.com>
2024-07-26 21:54:47 -0300

    Adição de footer versão mobile + atualização de header e footer large e header mobile

--- src/components/templates/small/FooterSmall.vue
+++ src/components/templates/small/FooterSmall.vue
@@ -21,18 +21,18 @@
     bottom: 0%;
     background-color: rgb(230, 230, 230);
     border-radius: 10px;
-    width: 99.2%;
-    height: 8vh;
+    width: 97%;
+    height: 5vh;
 }
 
 .links {
     display: flex;
-    width: 30%;
+    width: 100%;
     height: 100%;
     justify-content: center;
     align-items: center;
     gap: 4rem;
-    font-size: 1.2rem;
+    font-size:0.7rem;
 }
 
 .infos {
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      name: "home",
+      path: "/",
+      component: () => import('@/views/HomeView.vue'),
+
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: "acessorios",
+      path: "/acessorios",
+      component: () => import('@/views/AcessorioView.vue'),
+    },    {
+      name: "categorias",
+      path: "/categorias",
+      component: () => import('@/views/CategoriaView.vue'),
+    },    {
+      name: "cores",
+      path: "/cores",
+      component: () => import('@/views/CorView.vue'),
+    },    
+    {
+      name: "marcas",
+      path: "/marcas",
+      component: () => import('@/views/MarcaView.vue'),
+    },    
+    {
+      name: "modelos",
+      path: "/modelos",
+      component: () => import('@/views/ModeloView.vue'),
+    },    
+    {
+      name: "veiculos",
+      path: "/veiculos",
+      component: () => import('@/views/VeiculoView.vue'),
+    },
+    {
+      name: "login",
+      path: "/login",
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      name: "logout",
+      path: "/logout",
+      component: () => import('@/views/LogoutView.vue'),
     }
+
   ]
 })
 
